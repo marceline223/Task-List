5e2fb892677afd11d944dafbcf80965ffcbdd4e1
Dasha <dasha220301@gmail.com>
2023-04-11 19:37:00 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app>
+    <v-main class="v-main-container">
+      <current-task-list></current-task-list>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vuetify from "vuetify"
+import CurrentTaskList from "@/components/CurrentTaskList.vue"
 
 export default Vue.extend({
   name: 'App',
+  vuetify: new Vuetify(),
   components: {
-    HelloWorld
+    CurrentTaskList
+  },
+  data() {
+    return {
+      taskList: [
+        {
+          title: 'Задание 1',
+          items: ['Задача 1.1', 'Задача 1.2', 'Задача 1.3', 'Задача 1.4', 'Задача 1.5', 'Задача 1.6']
+        },
+        {
+          title: 'Задание 2',
+          items: ['Задача 2.1', 'Задача 2.2', 'Задача 2.3']
+        },
+        {
+          title: 'Задание 3',
+          items: ['Задача 3.1']
+        }
+      ]
+    }
   }
 });
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+.v-main-container {
+  width: 85%;
+  margin: auto;
 }
 </style>
---
+++ src/components/CurrentTaskList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h2 class="mt-4">Список заданий</h2>
+    <v-container fluid>
+      <v-row>
+        <v-btn class="mr-4" icon>
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+        <v-btn class="mr-4" icon disabled>
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-btn icon disabled>
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-row>
+    </v-container>
+    <v-simple-table density="comfortable">
+      <thead>
+      <tr>
+        <th>Задания</th>
+        <th>Задачи</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(task, index) in taskList" :key="index">
+        <td>{{ task.title }}</td>
+        <td>
+          <div v-for="(item, item_index) in task.items" :key="item_index">{{ item }}</div>
+        </td>
+      </tr>
+      </tbody>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CurrentTaskList",
+  data() {
+    return {
+      taskList: [
+        {
+          title: 'Задание 1',
+          items: ['Задача 1.1', 'Задача 1.2', 'Задача 1.3', 'Задача 1.4', 'Задача 1.5', 'Задача 1.6']
+        },
+        {
+          title: 'Задание 2',
+          items: ['Задача 2.1', 'Задача 2.2', 'Задача 2.3']
+        },
+        {
+          title: 'Задание 3',
+          items: ['Задача 3.1']
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
