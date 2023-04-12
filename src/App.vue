f1d269cb96555b49d3aad6498eaae781911f2c50
Dasha <dasha220301@gmail.com>
2023-04-12 03:03:34 +0300

    Remove unnecessary data

--- src/App.vue
+++ src/App.vue
@@ -16,24 +16,6 @@ export default Vue.extend({
   vuetify: new Vuetify(),
   components: {
     CurrentTaskList
-  },
-  data() {
-    return {
-      taskList: [
-        {
-          title: 'Задание 1',
-          items: ['Задача 1.1', 'Задача 1.2', 'Задача 1.3', 'Задача 1.4', 'Задача 1.5', 'Задача 1.6']
-        },
-        {
-          title: 'Задание 2',
-          items: ['Задача 2.1', 'Задача 2.2', 'Задача 2.3']
-        },
-        {
-          title: 'Задание 3',
-          items: ['Задача 3.1']
-        }
-      ]
-    }
   }
 });
 
