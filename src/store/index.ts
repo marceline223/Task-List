319277105d5767cbc370fa4423208be6853ab06a
Dasha <dasha220301@gmail.com>
2023-05-18 04:19:14 +0300

    Add async functions for adding, deleting, etc.

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,14 @@ import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate';
 
 import { Task, Item } from './store-types';
-import {GET_TASK_LIST} from "@/graphql/queries";
-import {apolloClient} from '../client/apollo'
+import {
+    ADD_NEW_TASK,
+    DELETE_TASK_BY_INDEX,
+    GET_TASK_LIST,
+    SET_STATUS_OF_ITEM,
+    SET_TASK_BY_INDEX
+} from "@/graphql/queries";
+import {apolloClient} from '@/client/apollo'
 
 Vue.use(Vuex)
 
@@ -27,7 +33,7 @@ export default new Vuex.Store({
             state.taskList[payload.indexForSetting] = payload.task;
         },
         SET_STATUS_OF_ITEM: (state, payload) => {
-            state.taskList[payload.taskIndex].itemList[payload.itemIndex].itemStatus = payload.status;
+            state.taskList[payload.taskIndex].itemList[payload.itemIndex].itemStatus = payload.itemStatus;
         },
         UPDATE_TASK_LIST: (state, payload) => {
             state.taskList = payload;
@@ -61,6 +67,35 @@ export default new Vuex.Store({
                 }
             }
         },
+        async deleteTaskByIndex({commit}, payload) {
+            const { data } = await apolloClient.query({query: DELETE_TASK_BY_INDEX});
+            if (data.deleteTaskByIndex.success) {
+                commit('DELETE_TASK_BY_INDEX', payload);
+                console.log(data.deleteTaskByIndex.message);
+            }
+        },
+        async addNewTask({commit}) {
+            const { data } = await apolloClient.query({query: ADD_NEW_TASK});
+            if (data.addNewTask.success) {
+                commit('ADD_NEW_TASK');
+            }
+            console.log(data.addNewTask.message);
+        },
+        async setTaskByIndex({commit}, payload) {
+            const { data } = await apolloClient.query({query: SET_TASK_BY_INDEX});
+            if (data.setTaskByIndex.success) {
+                commit('SET_TASK_BY_INDEX', payload);
+            }
+            console.log(data.setTaskByIndex.message);
+        },
+
+        async setStatusOfItem({commit}, payload) {
+            const { data } = await apolloClient.query({query:SET_STATUS_OF_ITEM});
+            if (data.setStatusOfItem.success) {
+                commit('SET_STATUS_OF_ITEM', payload);
+            }
+            console.log(data.setStatusOfItem.message);
+        }
     },
     plugins: [createPersistedState()],
 })
