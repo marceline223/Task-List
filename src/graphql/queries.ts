c392fadb9109d1d1c5189689a04e3ab206fbf5b0
Dasha <dasha220301@gmail.com>
2023-05-18 04:18:42 +0300

    Add all of current queries

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const GET_TASK_LIST = gql`
-  query getTaskList {
+  query Query {
     getTaskList {
       taskTitle
       itemList {
@@ -11,3 +11,39 @@ export const GET_TASK_LIST = gql`
     }
   }
 `;
+
+export const DELETE_TASK_BY_INDEX = gql`
+    query Query {
+      deleteTaskByIndex {
+        success
+        message
+      }
+    }
+`
+
+export const ADD_NEW_TASK = gql`
+    query Query {
+      addNewTask {
+        success
+        message
+      }
+    }
+`
+
+export const SET_TASK_BY_INDEX = gql`
+    query Query {
+      setTaskByIndex {
+        success
+        message
+      }
+    }
+`
+
+export const SET_STATUS_OF_ITEM = gql`
+    query Query {
+      setStatusOfItem {
+        success
+        message
+      }
+    }
+`
