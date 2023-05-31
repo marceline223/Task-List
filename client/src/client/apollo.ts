e0c11fdad8fe95fbed6d315c1e7589cd36e6a042
Dasha <dasha220301@gmail.com>
2023-05-31 13:39:58 +0300

    Resolve conflicting dependency

--- client/src/client/apollo.ts
+++ client/src/client/apollo.ts
@@ -1,8 +1,9 @@
-import VueApollo from 'vue-apollo'
-import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo';
+import {ApolloClient, InMemoryCache} from "@apollo/client/core";
 
 export const apolloClient = new ApolloClient({
-    uri: 'http://localhost:4000'
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache()
 })
 
 export const apolloProvider = new VueApollo({
